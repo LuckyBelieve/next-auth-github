b237bc59c9a6bf5120bd4d3c4ad16176fd7e4b4d
LuckyBelieve <luckybelieve@gmail.com>
2023-07-30 21:36:22 +0200

    github auth

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import { Poppins } from "next/font/google";
+import Navbar from "@/components/Navbar";
 
 const poppins = Poppins({
   weight: ["400", "700"],
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={poppins.className}>
+        <Navbar/>
         <main>{children}</main>
       </body>
     </html>
---
+++ components/Navbar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Navbar = () => {
+  return <nav className="w-full bg-black text-white p-5">this the navbar</nav>;
+};
+
+export default Navbar;
